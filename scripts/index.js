b017a1920201e4e4f4bd3196c472d33b3b01e452
NickFedko <nikitosfedkos@gmail.com>
2023-05-24 20:32:26 +0300

    add blur on menu clicked

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,9 @@
 let menuBtn = document.querySelector('.header__nav__menu__btn');
 let menu = document.querySelector('.header__nav__menu');
+let blur = document.querySelector('.blur');
 
 menuBtn.addEventListener('click', function() {
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
+	blur.classList.toggle('active');
 })
